b2a2786aa2365fb4fb8f15081ea6638ba88e25c1
Anatoliy <atsarkov.13@gmail.com>
2019-11-14 16:23:34 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+// однострочный комментарий
+
+
+/* комментарий для
+нескольких строк */
+
+//  ctrl+/
+
+
+/*var leftBorderWidth = 1;
+let second = 2;
+const p1 =3.14;
+console.log(second);*/
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "Kitti",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['pllum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+// alert('hello kitti');
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = prompt("Есть ли вам 18?", "Да");
+
+//  console.log(typeof(answer));
+
+// console.log(typeof(null));
+
+// console.log("arr" + "- object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
