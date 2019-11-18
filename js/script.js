651c48bb0828f3b5affc42d850102dce9f4b6b2a
Anatoliy <atsarkov.13@gmail.com>
2019-11-18 22:25:47 +0300

    lesson14

--- js/script.js
+++ js/script.js
@@ -1,24 +1,47 @@
-function first(){
-    // что то делаем
-    setTimeout( function(){
-        console.log(1);
-    }, 500);
+// function first(){
+//     // что то делаем
+//     setTimeout( function(){
+//         console.log(1);
+//     }, 500);
+// }
+
+// function second(){
+//     console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//     console.log("Я учу" + lang);
+//     callback();
+// }
+
+// function done() {
+//     console.log("Я прошел 3 урок!");
+// }
+
+// learnJS("Jawascript", done);
+
+
+let options =  {
+    width: 1024,
+    height: 1024,
+    name: "Kitti"
+};
+console.log(options.name);
+options.bool = false;
+options.colors = {
+    border: "black",
+    bg: "red"
+};
+
+delete options.bool;
+
+console.log(options);
+
+for (let key in options) {
+    console.log("Свойство " + key + " Имеет значение " + options[key]);
 }
 
-function second(){
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log("Я учу" + lang);
-    callback();
-}
-
-function done() {
-    console.log("Я прошел 3 урок!");
-}
-
-learnJS("Jawascript", done);
+console.log(Object.keys(options).length);
