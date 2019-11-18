ec7a2c401fab3808e6a3df5443b98fa253e6306f
Anatoliy <atsarkov.13@gmail.com>
2019-11-18 03:20:45 +0300

    lesson_13

--- js/script.js
+++ js/script.js
@@ -1,59 +1,24 @@
-'use strict';
+function first(){
+    // что то делаем
+    setTimeout( function(){
+        console.log(1);
+    }, 500);
+}
 
-// a = 5;
-// console.log(a);
+function second(){
+    console.log(2);
+}
 
-// однострочный комментарий
+first();
+second();
 
+function learnJS(lang, callback) {
+    console.log("Я учу" + lang);
+    callback();
+}
 
-/* комментарий для
-нескольких строк */
+function done() {
+    console.log("Я прошел 3 урок!");
+}
 
-//  ctrl+/
-
-
-/*var leftBorderWidth = 1;
-let second = 2;
-const p1 =3.14;
-console.log(second);*/
-
-console.log(4/0);
-console.log('string'*9);
-
-let something;
-console.log(something);
-
-let persone = {
-    name: "Kitti",
-    age: 25,
-    isMarried: false
-};
-
-console.log(persone["name"]);
-
-let arr = ['pllum.png','orange.jpg','apple.bmp'];
-
-console.log(arr[0]);
-
-// alert('hello kitti');
-
-// let answer = confirm("Are you here?");
-
-// console.log(answer);
-
-// let answer = prompt("Есть ли вам 18?", "Да");
-
-//  console.log(typeof(answer));
-
-// console.log(typeof(null));
-
-// console.log("arr" + "- object");
-
-let incr = 10,
-    decr = 10;
-
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
+learnJS("Jawascript", done);
