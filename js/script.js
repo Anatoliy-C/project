cff1f3015e3d6edf590f0b26f2ec024e9ff40ed3
Anatoliy <atsarkov.13@gmail.com>
2019-11-18 23:55:47 +0300

    lesson15

--- js/script.js
+++ js/script.js
@@ -23,25 +23,79 @@
 
 // learnJS("Jawascript", done);
 
+// Объекты 14
 
-let options =  {
-    width: 1024,
-    height: 1024,
-    name: "Kitti"
-};
-console.log(options.name);
-options.bool = false;
-options.colors = {
-    border: "black",
-    bg: "red"
-};
+// let options =  {
+//     width: 1024,
+//     height: 1024,
+//     name: "Kitti"
+// };
+// console.log(options.name);
+// options.bool = false;
+// options.colors = {
+//     border: "black",
+//     bg: "red"
+// };
 
-delete options.bool;
+// delete options.bool;
 
-console.log(options);
+// console.log(options);
 
-for (let key in options) {
-    console.log("Свойство " + key + " Имеет значение " + options[key]);
+// for (let key in options) {
+//     console.log("Свойство " + key + " Имеет значение " + options[key]);
+// }
+
+// console.log(Object.keys(options).length);
+
+// Массивы и псевдо массивы 15
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.pop();
+// arr.push("5"); 
+// arr.shift();    
+// arr.unshift("1"); 
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr[99] = 99;
+// console.log(arr.length);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     console.log(arr[i]);
+// }
+
+// let arr = ["first", 2, 3, "four", 5];
+
+// arr.forEach(function(item, i, mass){
+//     console.log(i + ': ' + item + "(массив: " + mass + ')');
+// });
+
+// console.log(arr);
+
+// let mass = [1,3,4,6,7]
+
+// for (let key of mass) {
+//     console.log(key);
+// }
+
+// let ans = prompt("", ""),
+//     arr = [];
+
+// arr =ans.split(',');
+// console.log(arr);
+
+// let arr = ['CSKA', 'Spartak', 'Dinamo', 'Locomotive'],
+//     i = arr.join(', ');
+// console.log(i);
+
+let arr = ['1', '15', '8', '4'],
+    i = arr.sort(compareNum);
+
+function compareNum(a,b) {
+    return a-b;
 }
 
-console.log(Object.keys(options).length);
+console.log(arr);
